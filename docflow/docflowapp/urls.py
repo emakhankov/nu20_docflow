48eb157f174593519dfc43217072f69dfdc10070
Jenya <emakhankov@gmail.com>
2020-03-09 16:10:58 +0300

    СBV формы шаблоны

--- docflow/docflowapp/urls.py
+++ docflow/docflowapp/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from docflowapp import views
 
 
-app_name = 'blogapp'
+app_name = 'docflowapp'
 
 urlpatterns = [
-    path('', views.index_view, name='index'),
-    path('search/', views.search_view, name='search'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('search', views.SearchView.as_view(), name='search'),
+    path('document/<int:pk>', views.DocumentView.as_view(), name='document_view'),
+    path('document/add', views.DocumentAdd.as_view(), name='document_add'),
 ]
